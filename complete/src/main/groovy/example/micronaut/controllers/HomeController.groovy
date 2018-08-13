df742ec17d42df13b1a8d7f2d9165ae9239cd85a
sdelamo <sergio.delamo@softamo.com>
2018-08-13 08:06:35 +0200

    Use Views Rendering

--- complete/src/main/groovy/example/micronaut/controllers/HomeController.groovy
+++ complete/src/main/groovy/example/micronaut/controllers/HomeController.groovy
@@ -1,11 +1,9 @@
 package example.micronaut.controllers
 
-import example.micronaut.services.VelocityService
-import io.micronaut.http.MediaType
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
-import io.micronaut.http.annotation.Produces
 import io.micronaut.security.Secured
+import io.micronaut.views.View
 
 import javax.annotation.Nullable
 import java.security.Principal
@@ -14,19 +12,9 @@ import java.security.Principal
 @Controller("/") // <2>
 class HomeController {
 
-    protected final VelocityService velocityService
-
-    HomeController(VelocityService velocityService) { // <3>
-        this.velocityService = velocityService
-    }
-
-    @Produces(MediaType.TEXT_HTML) // <4>
-    @Get("/") // <5>
-    String index(@Nullable Principal principal) { // <6>
-        velocityService.render("home.vm", homeModel(principal))
-    }
-
-    private Map homeModel(@Nullable Principal principal) {
+    @Get("/") // <3>
+    @View("home") // <4>
+    Map<String, Object>  index(@Nullable Principal principal) { // <5>
         Map<String, Object> data = ["loggedIn": principal!=null]
         if (principal) {
             data.username = principal.name
