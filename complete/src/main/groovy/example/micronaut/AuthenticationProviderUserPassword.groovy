2979b202fd997df6d40363fb2b0ed2238e551c3b
Sergio del Amo <sergio.delamo@softamo.com>
2020-06-22 15:03:12 +0200

    Update to 2.0.0.RC1

--- complete/src/main/groovy/example/micronaut/AuthenticationProviderUserPassword.groovy
+++ complete/src/main/groovy/example/micronaut/AuthenticationProviderUserPassword.groovy
@@ -1,28 +1,32 @@
-package example.micronaut.services
+package example.micronaut
 
 import edu.umd.cs.findbugs.annotations.Nullable
-import groovy.transform.CompileStatic
 import io.micronaut.http.HttpRequest
+import io.micronaut.security.authentication.AuthenticationException
 import io.micronaut.security.authentication.AuthenticationFailed
 import io.micronaut.security.authentication.AuthenticationProvider
 import io.micronaut.security.authentication.AuthenticationRequest
 import io.micronaut.security.authentication.AuthenticationResponse
 import io.micronaut.security.authentication.UserDetails
+import io.reactivex.BackpressureStrategy
 import io.reactivex.Flowable
 import org.reactivestreams.Publisher
 
 import javax.inject.Singleton
 
-@CompileStatic
 @Singleton // <1>
 class AuthenticationProviderUserPassword implements AuthenticationProvider { // <2>
 
     @Override
     Publisher<AuthenticationResponse> authenticate(@Nullable HttpRequest<?> httpRequest, AuthenticationRequest<?, ?> authenticationRequest) {
-        if (authenticationRequest.identity == "sherlock" && authenticationRequest.secret == "password") {
-            UserDetails userDetails = new UserDetails((String) authenticationRequest.identity, new ArrayList<>())
-            return Flowable.just(userDetails) as Flowable<AuthenticationResponse>
-        }
-        Flowable.just(new AuthenticationFailed()) as Flowable<AuthenticationResponse>
+        Flowable.create(emitter -> {
+            if (authenticationRequest.identity == "sherlock" && authenticationRequest.secret == "password") {
+                UserDetails userDetails = new UserDetails((String) authenticationRequest.getIdentity(), [])
+                emitter.onNext(userDetails)
+                emitter.onComplete()
+            } else {
+                emitter.onError(new AuthenticationException(new AuthenticationFailed()))
+            }
+        }, BackpressureStrategy.ERROR)
     }
 }
--- complete/src/main/groovy/example/micronaut/LoginAuthController.groovy
+++ complete/src/main/groovy/example/micronaut/LoginAuthController.groovy
@@ -1,4 +1,4 @@
-package example.micronaut.controllers
+package example.micronaut
 
 import groovy.transform.CompileStatic
 import io.micronaut.http.annotation.Controller
